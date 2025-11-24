3f75a3479d204308a104971b4363581f2e50e077
alexdorocode <alexdoro2024@gmail.com>
2025-11-24 15:44:01 +0100

    Update lookup bundle URL and filename in setup script

--- setup_and_test.sh
+++ setup_and_test.sh
@@ -25,8 +25,8 @@ echo "[1/4] Creating data/lookup directory..."
 mkdir -p data/lookup
 
 # Download the lookup bundle
-ZENODO_URL="https://zenodo.org/records/17630193/files/fantasia_lite_lookup_table.zip"
-BUNDLE_FILE="fantasia_lite_lookup_table.zip"
+ZENODO_URL="https://zenodo.org/records/17700779/files/fantasia_lite_data_folder.zip"
+BUNDLE_FILE="fantasia_lite_data_folder.zip"
 
 echo "[2/4] Downloading lookup bundle from Zenodo..."
 echo "URL: $ZENODO_URL"
